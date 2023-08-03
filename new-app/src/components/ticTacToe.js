50c1a9aadc19c0632db55232b812bdb48ae817ee
ekemlage <emilykemlage@gmail.com>
2023-08-03 15:18:00 -0500

    fixed table formatting and class id structure in css

--- new-app/src/components/ticTacToe.js
+++ new-app/src/components/ticTacToe.js
@@ -68,20 +68,20 @@ export default function TicTacToe(){
         <div>
             <table>
                 <tbody id="gameBoard">
-                    <tr id="row1">
-                        <td onClick={() => handleClick(0)} id="row1-column1" class ="column1 row1">{gameBoard[0]}</td>
-                        <td onClick={() => handleClick(1)} id="row1-column2" class ="column2 row1">{gameBoard[1]}</td>
-                        <td onClick={() => handleClick(2)}id="row1-column3" class ="column3 row1">{gameBoard[2]}</td>
+                    <tr>
+                        <td onClick={() => handleClick(0)} class ="gameBoard">{gameBoard[0]}</td>
+                        <td onClick={() => handleClick(1)} class ="column2 gameBoard">{gameBoard[1]}</td>
+                        <td onClick={() => handleClick(2)} class ="gameBoard">{gameBoard[2]}</td>
                     </tr>
                     <tr id="row2">
-                        <td onClick={() => handleClick(3)} id="row2-column1" class ="column1 row2">{gameBoard[3]}</td>
-                        <td onClick={() => handleClick(4)} id="row2-column2" class ="column2 row2">{gameBoard[4]}</td>
-                        <td onClick={() => handleClick(5)} id="row2-column3" class ="column3 row2">{gameBoard[5]}</td>
+                        <td onClick={() => handleClick(3)} class ="gameBoard row2">{gameBoard[3]}</td>
+                        <td onClick={() => handleClick(4)} class ="column2 gameBoard row2">{gameBoard[4]}</td>
+                        <td onClick={() => handleClick(5)} class ="gameBoard row2">{gameBoard[5]}</td>
                     </tr>
-                    <tr id="row3">
-                        <td onClick={() => handleClick(6)} id="row3-column1" class ="column1 row3">{gameBoard[6]}</td>
-                        <td onClick={() => handleClick(7)} id="row3-column2" class ="column2 row3">{gameBoard[7]}</td>
-                        <td onClick={() => handleClick(8)} id="row3-column3" class ="column3 row3">{gameBoard[8]}</td>
+                    <tr>
+                        <td onClick={() => handleClick(6)} class ="gameBoard">{gameBoard[6]}</td>
+                        <td onClick={() => handleClick(7)} class ="column2 gameBoard">{gameBoard[7]}</td>
+                        <td onClick={() => handleClick(8)} class ="gameBoard">{gameBoard[8]}</td>
                     </tr>
                     <tr>
                         <th colspan="3">{turnMessage}</th>
